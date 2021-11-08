58de180499f5709e91eca7dc8b4a0230a8682069
Leonardo Galhardo <leomgal20@hotmail.com>
2021-11-08 15:38:25 -0300

    changing the place where the condition was.

--- R/pandemic_model.R
+++ R/pandemic_model.R
@@ -845,8 +845,11 @@ pandemic_model <- function(Y, case_type = "confirmed",family="poisson", seasonal
   } else if(!("new_deaths" %in% names(Y$data)) && "new_cases" %in% names(Y$data) && is.numeric(Y$data$new_cases)){
     data_cases <- TRUE
     Y$data <- cbind(Y$data, new_deaths = Y$data$new_cases)
-  } else if(!any(c("new_cases", "new_deaths") %in% names(Y$data))) {
-      if("cases" %in% names(Y$data)){
+  }}
+
+  # Y$data without either 'new_deaths' and 'new_cases':
+  if(!any(c("new_cases", "new_deaths") %in% names(Y$data))) {
+    if("cases" %in% names(Y$data)){
       n_lines = nrow(Y$data)
       col_new_cases = c()
       i == 1
@@ -865,7 +868,7 @@ pandemic_model <- function(Y, case_type = "confirmed",family="poisson", seasonal
         i = i+1
       }
       Y$data["new_cases"] = c(col_new_cases)
-  } else{
+    } else{
       n_lines <- nrow(Y$data)
       col_new_deaths <- c()
       i == 1
